1862895ef420f17857b1e28b038bb459d2fb4262
lcg0211 <403060884@qq.com>
2015-08-27 16:39:10 +0800

    增加配置文件来配置调度的时间规则
    Signed-off-by: lcg0211 <403060884@qq.com>

--- src/com/icss/schedule/SchedMain.java
+++ src/com/icss/schedule/SchedMain.java
@@ -4,6 +4,8 @@ import static org.quartz.JobBuilder.*;
 import static org.quartz.TriggerBuilder.*;
 import static org.quartz.CronScheduleBuilder.*;
 
+import java.util.Properties;
+
 import org.quartz.CronTrigger;
 import org.quartz.JobDetail;
 import org.quartz.Scheduler;
@@ -11,29 +13,36 @@ import org.quartz.impl.StdSchedulerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.icss.jdbc.JdbcUtils;
+
 public class SchedMain {
 	@SuppressWarnings("unused")
 	private static Logger log = LoggerFactory.getLogger(SchedMain.class);
 	private static Scheduler sched;
 
 	public static void run() throws Exception {
+		Properties pt = new Properties();
+		pt.load(SchedMain.class.getResourceAsStream("/../config/quartz.properties")); //路径为 WEB-INF/config/quartz.properties
+		String cronSchedule1 = pt.getProperty("JDOAuthRefreshTokenJob");
+		String cronSchedule2 = pt.getProperty("JDGetDeliveryNumJob");
+		String cronSchedule3 = pt.getProperty("JDSendDeliveryInfoJob");
 		JobDetail job1 = newJob(JDOAuthRefreshTokenJob.class).withIdentity("authJob1",
 				"group1").build();
 		CronTrigger trigger1 = newTrigger()
 				.withIdentity("authTrigger1", "group1")
-				.withSchedule(cronSchedule("* 0/30 * * * ?"))
+				.withSchedule(cronSchedule(cronSchedule1))
 				.forJob("authJob1", "group1").build();
 		JobDetail job2 = newJob(JDGetDeliveryNumJob.class).withIdentity("expressJob1",
 				"group1").build();
 		CronTrigger trigger2 = newTrigger()
 				.withIdentity("expressTrigger1", "group1")
-				.withSchedule(cronSchedule("0/30 * * * * ?"))
+				.withSchedule(cronSchedule(cronSchedule2))
 				.forJob("expressJob1", "group1").build();
 		JobDetail job3 = newJob(JDSendDeliveryInfoJob.class).withIdentity("expressJob2",
 				"group1").build();
 		CronTrigger trigger3 = newTrigger()
 				.withIdentity("expressTrigger2", "group1")
-				.withSchedule(cronSchedule("0/30 * * * * ?"))
+				.withSchedule(cronSchedule(cronSchedule3))
 				.forJob("expressJob2", "group1").build();
 		sched = StdSchedulerFactory.getDefaultScheduler();
 		sched.scheduleJob(job1, trigger1);
